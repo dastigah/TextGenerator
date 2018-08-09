ca7a02c13537c26ba2c1632b890d160b7f47589a
Tony <tony.pham@uconn.edu>
2018-08-09 08:45:36 +0000

    #4 feature/KeyListClass: Add constructor, and getters for class. Working on parsing file for mapped words

--- KeyList.cpp
+++ KeyList.cpp
@@ -1,1 +1,35 @@
 #include "KeyList.H"
+
+#include<ifstream>
+
+using namespace std;
+
+void parseMappedWords(){
+	ifstream inputFile(fileName);
+
+	if (!inputFile.is_open()){	
+		throw(string("Unable to open " + fileName));
+	}
+
+	string line;
+	while (getline(inputFile,line)){
+
+	}
+
+
+}
+
+KeyList::KeyList(string _fileName): fileName(_fileName){
+	try{
+		parseMappedWords();
+	}
+	catch(string & e){
+		cout << "Error: " + e << endl;
+	}
+}
+
+string KeyList::getFileName() {return fileName;}
+
+string KeyList::getKeyPhrase(const string & input){
+	return mappedWords[input];
+}
