9e430307f9b0bb4ed6538816c11a06a0566daee0
Tony Pham <tony.pham@uconn.edu>
2018-08-14 22:02:59 -0400

    #4 : Add statement to stringSplit to not include empty string in vector

--- stringUtils.cpp
+++ stringUtils.cpp
@@ -9,6 +9,7 @@ vector<string> stringSplit(const string & input ,const string & delimiter){
 	
 	while (begin != string::npos){ // While not at the end of the string
 		foundIndex = input.find(delimiter,begin);
+		if (begin == foundIndex) continue; //Dont add empty string to list
 		splitWords.push_back(string(begin,foundIndex));
 		begin = (foundIndex == string::npos) ? (string::npos) : foundIndex + delimiter.length(); //if not end of string move pointer pass the delimiter
 	}
