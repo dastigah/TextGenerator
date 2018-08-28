4a62456a45ffac9ef8b61c240db4ef7f6a82f3bd
Tony Pham (desktop) <tony.pham@uconn.edu>
2018-08-27 22:47:29 -0400

    #11: fix to previous reset of done work

--- test/KeyListTest.cpp
+++ test/KeyListTest.cpp
@@ -1,11 +1,44 @@
 #include "KeyListTest.H"
+#include "KeyList.H"
+
+using namespace std;
 
 CPPUNIT_TEST_SUITE_REGISTRATION( KeyListTest );
 
 void KeyListTest::setUp(void){
 	//TODO
+	testSingleFn = "testFiles/testSingle.txt";
+	testMultipleFn = "testFiles/testMultiple.txt";
+	testComboFn = "testFiles/testCombo.txt";
+	testInvalidSeparatorFn = "testFiles/testInavlidSeparator.txt";		
+
+	testSingleMap = {	{"cat","dog"},
+						{"chicken","pig"},
+						{"expeditition","voyage"},
+						{"fire","water"}
+					}; 
+				
+						
+	
+
 }
 
 void KeyListTest::tearDown(void){
 	//TODO
+}
+
+void KeyListTest::testSingleWords(void){
+	KeyList actual = KeyList(testSingleFn);
+	CPPUNIT_ASSERT( mapComparator(testSingleMap,actual.getMap()) );
+}
+
+bool KeyListTest::mapComparator(map<string,string> & t1, map<string,string> & t2){
+	for (auto it : t1){
+		if(t2[it.first] != it.second) return false;	
+	}
+	for (auto it : t2){
+		if (t1[it.first] != it.second) return false;
+	}
+
+	return true;
 }
